757bbf86d27358606eb27ac7fa38e687db59373e
Diana Nnaemeka <104990762+dev-diana@users.noreply.github.com>
2023-09-21 20:17:36 +0100

    component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+
+
+export default function Footer() {
+  return (
+    <footer className="text-center md:text-start">
+      <hr className="md:w-full w-[50%]"/>
+      <p>© <a href="https://x.com/devdiana0x" rel="noreferrer">Diana Nnaemeka</a></p>
+    </footer>
+  )
+}
