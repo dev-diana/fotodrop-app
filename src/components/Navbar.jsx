06fe9fe358fbbea1f08010e68ba3924b39b348b1
Diana Nnaemeka <104990762+dev-diana@users.noreply.github.com>
2023-09-21 20:25:50 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,9 @@
 import { Link, useNavigate } from "react-router-dom";
-import logo from "../assets/logo.svg"
+
 
 export default function NavBar() {
 
   return (
-    <Link to={`/`} className="flex items-center gap-2 text-lg md:text-xl font-bold"><img src={logo} alt="Logo" />FotoDrop</Link>
+    <Link to={`/`} className="flex items-center gap-2 text-lg md:text-xl font-bold"><img src="" alt="Logo" />FotoDrop</Link>
   )
 }
